31686d1bc14bb5f725bbbc557ffa4342a505c53c
itahmedosama-dotcom <it.ahmedosama@gmail.com>
2026-08-27 13:12:29 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.SITE_CONFIG = {
   // ضع رابط Web App بعد نشر Google Apps Script هنا.
-  appsScriptUrl: "https://script.google.com/macros/s/AKfycbyGSCAJJQATbEiMryVrFya6qtMqbhbui9VvNIdzOTVBPc_W-rZJt9sigCoyEcldk4merw/exec",
+  appsScriptUrl: "https://script.google.com/macros/s/AKfycbx8EOyyw29rtK2cjyeXhIEParDbwf8baqXohzJfi1GlG9fdWrbEJTY4_8C8R91krwwA7w/exec",
   whatsapp: "966540394555",
   linktree: "https://linktr.ee/alsiteen60",
   snapchat: "https://www.snapchat.com/@alsieen60"
