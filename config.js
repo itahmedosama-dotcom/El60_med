569cee4c3795ddecd3367443f2f4ec1f7fe09ae0
itahmedosama-dotcom <it.ahmedosama@gmail.com>
2026-08-27 12:38:44 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+window.SITE_CONFIG = {
+  // ضع رابط Web App بعد نشر Google Apps Script هنا.
+  appsScriptUrl: "https://script.google.com/macros/s/AKfycbyGSCAJJQATbEiMryVrFya6qtMqbhbui9VvNIdzOTVBPc_W-rZJt9sigCoyEcldk4merw/exec",
+  whatsapp: "966540394555",
+  linktree: "https://linktr.ee/alsiteen60",
+  snapchat: "https://www.snapchat.com/@alsieen60"
+};
