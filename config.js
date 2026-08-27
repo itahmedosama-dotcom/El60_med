a010644a80cdf1f3078d39f2fba187902a49831d
itahmedosama-dotcom <it.ahmedosama@gmail.com>
2026-08-27 13:43:30 +0300

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-window.SITE_CONFIG = {
-  // ضع رابط Web App بعد نشر Google Apps Script هنا.
-  appsScriptUrl: "https://script.google.com/macros/s/AKfycbx8EOyyw29rtK2cjyeXhIEParDbwf8baqXohzJfi1GlG9fdWrbEJTY4_8C8R91krwwA7w/exec",
-  whatsapp: "966540394555",
-  linktree: "https://linktr.ee/alsiteen60",
-  snapchat: "https://www.snapchat.com/@alsieen60"
-};
